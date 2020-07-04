b76e0b6aa64b942737fd2d807520c3650ba10561
David Hubber <david.hubber@icloud.com>
2020-07-04 01:16:45 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+ 
+
+ 	$("#stream1_btn").click(function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").mouseenter(function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").mouseleave(function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+
+
+}); 
